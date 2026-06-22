475919bcd51ad5a329aa1365369db2557062b4e0
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-06-22 21:02:44 +0530

    created health.py

---
+++ app/api/routes/health.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/health",
+    tags=["Health"]
+)
+
+
+@router.get("/")
+async def health():
+
+    return {
+        "status": "healthy"
+    }
