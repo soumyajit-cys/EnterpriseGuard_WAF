0421d3215f0287a64432fed34c9ad6c321e9817e
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-06-27 19:24:42 +0530

    created alerts.py

---
+++ app/api/routes/alerts.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/alerts",
+    tags=["Alerts"]
+)
+
+
+@router.get("/")
+async def list_alerts():
+
+    return {
+        "alerts": []
+    }
+
