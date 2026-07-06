c60f2c6d1dc0b3f8ba7af645391eac68f6201d8f
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-07-06 08:35:39 +0530

    updated security_headers.py

--- app/core/security_headers.py
+++ app/core/security_headers.py
@@ -1,18 +1,18 @@
 def add_security_headers(response):
 
     response.headers["X-Frame-Options"] = "DENY"
-
     response.headers["X-Content-Type-Options"] = "nosniff"
-
     response.headers["Referrer-Policy"] = "strict-origin"
 
     response.headers["Content-Security-Policy"] = (
-        "default-src 'self'"
+        "default-src 'self'; "
+        "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; "
+        "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; "
+        "img-src 'self' data: https://fastapi.tiangolo.com;"
     )
 
     response.headers["Permissions-Policy"] = (
         "camera=(), microphone=()"
     )
-
 
     
