0164ca91542fc45b44fed2e5b9c5c05ff007926c
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-08-16 08:15:46 +0530

    fix: Ensure existing loggers are not disabled when configuring logging

--- alembic/env.py
+++ alembic/env.py
@@ -22,7 +22,7 @@ import app.models.audit_log  # noqa: F401
 config = context.config
 
 if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+    fileConfig(config.config_file_name, disable_existing_loggers=False)
 
 config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
