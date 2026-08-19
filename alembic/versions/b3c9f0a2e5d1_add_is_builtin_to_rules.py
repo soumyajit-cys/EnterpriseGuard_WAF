77fa7a809df3f2e872866f050eade7f2d7e2b943
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-08-19 07:39:07 +0530

    fix: update down_revision to correct migration reference

--- alembic/versions/b3c9f0a2e5d1_add_is_builtin_to_rules.py
+++ alembic/versions/b3c9f0a2e5d1_add_is_builtin_to_rules.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "b3c9f0a2e5d1"
-down_revision: Union[str, None] = "d27aa81d07e2"
+down_revision: Union[str, None] = "9f2e7c1a4b8d"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
